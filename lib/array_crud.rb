70dccfb0d516188e6e8bbb7266a96da814bee8d4
Emily Lillie <emilylillie3@gmail.com>
2019-07-17 00:57:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["Quentin", "James", "Frances", "Violet"] << "Lucille"
 end
 
 def add_element_to_start_of_array(array, element)
